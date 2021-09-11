58cdd63e225cf98f3a06f86f1360f6ac6a7b5db8
quamilou <50031737+quamilou@users.noreply.github.com>
2021-09-11 20:36:25 +0200

    <message> <date/heure> SaQu

---
+++ src/world.java
@@ -0,0 +1,9 @@
+public class world {
+    public static void main(String[] args) {
+        int note1=10; int note2=0; int note3=0;
+
+        int moyenne=(note1+note2+note3)/3;
+        System.out.println("ma moyenne est:"+moyenne);
+
+    }
+}
