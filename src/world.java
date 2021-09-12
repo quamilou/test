6bd6fd26bfb29d82585802dfafb69675dc33d0dc
quamilou <50031737+quamilou@users.noreply.github.com>
2021-09-13 00:14:14 +0200

    <moyenne de 3 note> <13/09;00:13> SaQu

--- src/world.java
+++ src/world.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class world {
     public static void main(String[] args) {
-        int note1=10; int note2=0; int note3=0;
+        //int note1=10; int note2=0; int note3=0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("entrez note1: ");
+        int note1 = sc.nextInt();
+
+        System.out.print("entrez note2: ");
+        int note2 = sc.nextInt();
+
+        System.out.print("entrez note3: ");
+        int note3 = sc.nextInt();
 
         int moyenne=(note1+note2+note3)/3;
-        System.out.println("ma moyenne est:"+moyenne);
+        System.out.println("ta moyenne est de: "+moyenne);
 
     }
 }
